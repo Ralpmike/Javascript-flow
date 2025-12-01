9f82208e51470967b8ab316b0501601ad8396c11
Raphael Michael <ralphmike996@gmail.com>
2025-12-01 17:22:51 +0100

    initial commit: guessing game

--- Guessing game/script.js
+++ Guessing game/script.js
@@ -1,9 +1,19 @@
-const seccretNumber = Math.floor(Math.random() * 20) + 1;
-const message = document.querySelector(".message")
+"use strict"
 
-console.log(message);
+let seccretNumber = Math.floor(Math.random() * 20) + 1;
+let chances = 10 ;
+let message = document.querySelector(".message")
+document.querySelector("#chances").innerHTML = `You have <span class="chance">${chances}</span> chances to guess the number`
+
+
+
+// console.log(message.style.fontSize = " 100px");
+// message.style.color = "green";
+// message.style.backgroundColor = "pink"
+// message.style.padding = "20px"
+// message.style.borderRadius = "15px"
+// message.style.display = "none"
 console.log("Seccret number is: ", seccretNumber);
-
 
 
 
@@ -12,16 +22,59 @@ document.querySelector(".submit").addEventListener("click", function() {
     console.log("Number guessed is: ",  numberGuessed);
 
     if(numberGuessed === 0){
-        message.textContent  = "Number is required 😵";    
+        message.textContent  = "Number is required 😵";  
+        
     }else if(numberGuessed > seccretNumber){
+        if(chances > 1 ){
+            chances--;
+            document.querySelector(".submit").disabled = false;
+        }else{
+            chances--;
+            document.querySelector(".submit").disabled = true;
+            document.querySelector("#chances").innerHTML = `You have <span class="chance">${chances}</span> chances to guess the number`
+            message.textContent  = `Game  over! ☠️ ☠️ ☠️`;
+
+            return
+        }
+
         message.textContent  = "Guess Too high 📈";
+        document.querySelector("#chances").innerHTML = `You have <span class="chance">${chances}</span> chances to guess the number`
+
+
+        message.style.display = "block"
     }else if(numberGuessed < seccretNumber){
+        if (chances > 1) {
+            chances--;
+            document.querySelector(".submit").disabled = false;
+        }else{
+            chances--;
+            document.querySelector("#chances").innerHTML = `You have <span class="chance">${chances}</span> chances to guess the number`
+            document.querySelector(".submit").disabled = true;
+            message.textContent = `Game  over! 💥 💥 💥`;
+            return
+        }
+
         message.textContent  = "Guess Too low 📉";
+        document.querySelector("#chances").innerHTML = `You have <span class="chance">${chances}</span> chances to guess the number`
+
+
     }else if(numberGuessed === seccretNumber){
         message.textContent  = "Congratulations! You guessed it right 🎉🎊";
         document.querySelector(".show_secret_number").textContent = seccretNumber;
-        document.body.style.backgroundColor = "#60b347";
+        document.body.style.backgroundColor = "#164a07ff";
     }
+
+})
+
+document.querySelector(".reset").addEventListener("click", function() {
+  seccretNumber = Math.floor(Math.random() * 20) + 1;
+    message.textContent = ""
+    document.body.style.backgroundColor = '#222'
+    document.querySelector("#number").value = ""
+    document.querySelector(".show_secret_number").textContent = "?"
+    document.querySelector(".submit").disabled = false;
+    chances = 5;
+    document.querySelector("#chances").innerHTML = `You have <span class="chance">${chances}</span> chances to guess the number`
 
 })
 
@@ -29,7 +82,7 @@ document.querySelector(".submit").addEventListener("click", function() {
 // Coding Challenge #1
 
 /* 
-Implement a game rest functionality, so that the player can make a new guess! Here is how:
+Implement a game reset functionality, so that the player can make a new guess! Here is how:
 
 1. Select the element with the 'reset' class and attach a click event handler
 2. In the handler function, restore initial values of the secretNumber variable
